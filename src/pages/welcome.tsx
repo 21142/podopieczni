0feb670a0cb03d7e4945bfce93292975017b9bac
Maciej Galiński <s21142@pjwstk.edu.pl>
2022-11-28 16:37:09 +0100

    role handling after logging in

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -2,10 +2,18 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { LinkButton } from 'src/components/buttons/link/LinkButton';
 import PageLayout from 'src/components/layouts/primary/PageLayout';
+import Landing from 'src/components/pages/landing/Landing';
 import { trpc } from 'src/utils/trpc';
 
 const Welcome: NextPage = () => {
   const router = useRouter();
+
+  const isLoggingInQuery = trpc.auth.getSession.useQuery(undefined);
+  const hasRoleQuery = trpc.user.hasRole.useQuery(undefined);
+
+  const isLoggingIn = isLoggingInQuery.data;
+  const hasRole = hasRoleQuery.data;
+
   const { mutateAsync: setRoleAsAdopter } =
     trpc.auth.setAdoptingRole.useMutation();
   const { mutateAsync: setRoleAsShelter } =
@@ -25,19 +33,27 @@ const Welcome: NextPage = () => {
   return (
     <>
       <PageLayout>
-        <div className="grid h-144 place-items-center content-center">
-          <p className="text-lg">Please, tell us what are you here for</p>
-          <div className="flex gap-5 p-5">
-            <LinkButton
-              value="I want to adopt a pet"
-              onClick={setAdopterRole}
-            />
-            <LinkButton
-              value="I work in a shelter"
-              onClick={setShelterRole}
-            />
-          </div>
-        </div>
+        {isLoggingIn ? (
+          hasRole ? (
+            <div>HERE IS TEST VIEW FOR USERS WITH ASSIGNED ROLE</div>
+          ) : (
+            <div className="grid h-80 place-items-center content-center">
+              <p className="text-lg">Please, tell us what are you here for</p>
+              <div className="flex gap-5 p-5">
+                <LinkButton
+                  value="I want to adopt a pet"
+                  onClick={setAdopterRole}
+                />
+                <LinkButton
+                  value="I work in a shelter"
+                  onClick={setShelterRole}
+                />
+              </div>
+            </div>
+          )
+        ) : (
+          <Landing />
+        )}
       </PageLayout>
     </>
   );
