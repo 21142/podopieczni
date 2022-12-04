7c6f1918620426d1be170123336208aa78be9a2c
Maciej Galiński <s21142@pjwstk.edu.pl>
2022-12-04 19:12:40 +0100

    updated auth button for mobile and cleaned up some code

--- src/components/buttons/auth/AuthButton.tsx
+++ src/components/buttons/auth/AuthButton.tsx
@@ -3,16 +3,16 @@ import Avatar from 'src/components/avatars/base/Avatar';
 import { trpc } from 'src/utils/trpc';
 
 const AuthButton: React.FC = () => {
-  const query = trpc.auth.getSession.useQuery(undefined);
+  const query = trpc.auth.getSession.useQuery();
   const sessionData = query.data;
 
   return (
     <button
-      className="duration-50 peer cursor-pointer rounded-full border border-primary-300 py-1 px-4 text-sm font-medium text-primary-300 duration-300 ease-in-out hover:scale-95 hover:bg-primary-300 hover:font-semibold hover:text-white lg:text-base"
+      className="duration-50 lg:text-md min-w-[8rem] cursor-pointer rounded-full border border-primary-300 py-1 px-4 text-sm font-medium text-primary-300 duration-300 ease-in-out hover:scale-95 hover:bg-primary-300 hover:font-semibold hover:text-white xl:text-base"
       onClick={sessionData ? () => signOut() : () => signIn()}
     >
       {sessionData ? (
-        <div className="flex items-center gap-2">
+        <div className="flex min-w-[7.5rem] items-center justify-between xl:gap-2">
           Wyloguj się{' '}
           <Avatar
             src={sessionData?.image ?? '/no-profile-picture.svg'}
--- src/components/buttons/link/LinkButton.tsx
+++ src/components/buttons/link/LinkButton.tsx
@@ -10,7 +10,7 @@ export const LinkButton = ({
   return (
     <button
       onClick={onClick}
-      className={`flex cursor-pointer items-center gap-2 rounded-full border border-primary-300 py-1 px-4 text-sm text-primary-300 transition-colors duration-200 ease-in-out hover:bg-primary-300 hover:text-white lg:text-lg ${className}`}
+      className={`lg:text-md flex cursor-pointer items-center justify-center gap-2 rounded-full border border-primary-300 py-1 px-4 text-sm text-primary-300 transition-colors duration-200 ease-in-out hover:bg-primary-300 hover:text-white ${className}`}
     >
       {value}
     </button>
