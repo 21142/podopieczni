3871be33546a835c65b5f5b565be133f9c1cb57d
Maciej Galiński <s21142@pjwstk.edu.pl>
2022-12-08 22:38:20 +0100

    Added custom og:image and other open graph tags

---
+++ src/pages/api/og-image.tsx
@@ -0,0 +1,51 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async function () {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'white',
+          position: 'relative',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          tw="w-full h-[60%] top-0 absolute"
+          style={{ transform: 'rotate(180deg)' }}
+          src={`${process.env.NEXT_PUBLIC_BASE_URL}/images/footer-wave.svg`}
+          alt="podopieczni logo"
+        />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          tw="w-full h-[50%] bottom-0 absolute"
+          src={`${process.env.NEXT_PUBLIC_BASE_URL}/images/footer-wave.svg`}
+          alt="podopieczni logo"
+        />
+        <div tw="flex flex-col items-center text-center pt-5">
+          <h1 tw="text-[52px] mb-5">
+            Szukaj podopiecznych gotowych do adopcji.
+          </h1>
+          <p tw="text-[32px]">
+            Pomagamy w adopcji i zarządzaniu danymi zwierząt.
+          </p>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
