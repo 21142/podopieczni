c892f62b4a726bd450f99cc4c6d18a1b70055bdf
21142 <s21142@pjwstk.edu.pl>
2023-05-08 22:23:35 +0200

    set number of runs for every audit to 3

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,11 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'NODE_ENV=production npm run start',
       url: [
         'https://podopieczni-21142.vercel.app/',
         'https://podopieczni-dev.vercel.app/',
       ],
-      numberOfRuns: 1,
+      numberOfRuns: 3,
     },
     upload: {
       target: 'temporary-public-storage',
