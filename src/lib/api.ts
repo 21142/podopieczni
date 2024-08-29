b2b5d69024b9e22916fc5ac6847c0053dd6e8bff
21142 <s21142@pjwstk.edu.pl>
2024-08-29 16:28:36 +0200

    Enable logger only if environment is not prod

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -12,8 +12,9 @@ export const api = createTRPCNext<AppRouter>({
       links: [
         loggerLink({
           enabled: (opts) =>
-            process.env.NODE_ENV === 'development' ||
-            (opts.direction === 'down' && opts.result instanceof Error),
+            process.env.NODE_ENV !== 'production' &&
+            opts.direction === 'down' &&
+            opts.result instanceof Error,
         }),
         httpBatchLink({
           transformer: superjson,
